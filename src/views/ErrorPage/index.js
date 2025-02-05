02acb244b9f574788e849e0647dcbca398e22a13
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 15:01:19 +0530

    Create index.js

---
+++ src/views/ErrorPage/index.js
@@ -0,0 +1,6 @@
+import ErrorPage from './ErrorPage';
+import NotFound from './NotFound';
+import ServerError from './ServerError';
+import Maintenance from './Maintenance';
+
+export { ErrorPage, NotFound, ServerError, Maintenance };
