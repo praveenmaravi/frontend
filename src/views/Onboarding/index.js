6029f7d05f95c762c8b0699fc25023ca373cc1ba
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 20:54:10 +0530

    Delete src/views/Onboarding directory

--- src/views/Onboarding/index.js
+++
@@ -1,6 +0,0 @@
-// src/views/Onboarding/index.js
-export { default as FeatureTutorial } from './FeatureTutorial';
-export { default as IndustrySelection } from './IndustrySelection';
-export { default as OnboardingComplete } from './OnboardingComplete';
-export { default as OnboardingSteps } from './OnboardingSteps';
-export { default as PreferencesSetup } from './PreferencesSetup';
