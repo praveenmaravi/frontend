1e4cb039e8dd178b958caee32b0044c32ffbdbfc
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 15:05:22 +0530

    Update index.js

--- src/views/ErrorPage/index.js
+++ src/views/ErrorPage/index.js
@@ -1,6 +1,5 @@
 import ErrorPage from './ErrorPage';
 import NotFound from './NotFound';
-import ServerError from './ServerError';
 import Maintenance from './Maintenance';
 
-export { ErrorPage, NotFound, ServerError, Maintenance };
+export { ErrorPage, NotFound,  Maintenance };
