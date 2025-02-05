f1816d904c099de6d3f2d0b3f08684cda226b1cc
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 23:49:39 +0530

    Create MarketplaceContainer.js

---
+++ src/views/Marketplace/MarketplaceContainer.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import MarketplacePage from "./MarketplacePage";
+import { useMarketplaceItems } from "../hooks/useMarketplaceItems";
+import { useCategoryFilter } from "../hooks/useCategoryFilter";
+import { useSortItems } from "../hooks/useSortItems";
+import { useSearch } from "../hooks/useSearch";
+
+const MarketplaceContainer = () => {
+  const { items, fetchItems, loading } = useMarketplaceItems();
+  const { filteredItems, filterByCategory } = useCategoryFilter(items);
+  const { sortedItems, sortItems } = useSortItems(filteredItems);
+  const { searchedItems, searchQuery, setSearchQuery } = useSearch(sortedItems);
+
+  useEffect(() => {
+    fetchItems();
+  }, [fetchItems]);
+
+  return (
+    <MarketplacePage
+      items={searchedItems}
+      loading={loading}
+      onCategoryFilter={filterByCategory}
+      onSortChange={sortItems}
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+    />
+  );
+};
+
+export default MarketplaceContainer;
