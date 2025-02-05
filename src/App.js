5933aaa7e8f63beca89b9dd6ecdb54ec55067fe6
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 16:27:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import Authentication from "views/Auth/Authentication";
 import { loadUserData } from './store/actions/userActions';
 
 // Importing Global Styles
-@import "@/assets/styles/base/_global.scss";
+import "@/assets/styles/base/_global.scss";
 
 
 const App = () => {
