55008ec98db9bd2f66caf65376f71c4bb65d8129
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 20:39:31 +0530

    Create index.js

---
+++ src/views/Onboarding/index.js
@@ -0,0 +1,6 @@
+// src/views/Onboarding/index.js
+export { default as FeatureTutorial } from './FeatureTutorial';
+export { default as IndustrySelection } from './IndustrySelection';
+export { default as OnboardingComplete } from './OnboardingComplete';
+export { default as OnboardingSteps } from './OnboardingSteps';
+export { default as PreferencesSetup } from './PreferencesSetup';
