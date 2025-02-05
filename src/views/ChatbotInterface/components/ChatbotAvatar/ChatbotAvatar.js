a59e7801c5dfd7d725b88785d546c5ecb6159c96
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-05 23:15:56 +0530

    Create ChatbotAvatar.js

---
+++ src/views/ChatbotInterface/components/ChatbotAvatar/ChatbotAvatar.js
@@ -0,0 +1,24 @@
+// frontend/src/views/ChatbotInterface/components/ChatbotAvatar/ChatbotAvatar.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ChatbotAvatar.css';
+
+const ChatbotAvatar = ({ avatarUrl, altText }) => {
+  return (
+    <div className="chatbot-avatar">
+      <img
+        src={avatarUrl || '/default-avatar.png'} // Default avatar if none is provided
+        alt={altText || 'Chatbot Avatar'}
+        className="chatbot-avatar-img"
+      />
+    </div>
+  );
+};
+
+ChatbotAvatar.propTypes = {
+  avatarUrl: PropTypes.string,
+  altText: PropTypes.string,
+};
+
+export default ChatbotAvatar;
