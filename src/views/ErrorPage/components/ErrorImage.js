949477d772a68233e65b4fb8ce4e9d99a8e259cc
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 01:08:57 +0530

    Create ErrorImage.js

---
+++ src/views/ErrorPage/components/ErrorImage.js
@@ -0,0 +1,12 @@
+// frontend/src/views/ErrorPage/components/ErrorImage.js
+import React from 'react';
+
+const ErrorImage = () => {
+  return (
+    <div className="flex justify-center mt-4">
+      <img src="/images/error.svg" alt="Error illustration" className="h-40 w-auto" />
+    </div>
+  );
+};
+
+export default ErrorImage;
