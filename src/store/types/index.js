07b3cab15e142100ae79715f3ab3d56de43fc4f7
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 12:23:46 +0530

    Create index.js

---
+++ src/store/types/index.js
@@ -0,0 +1,11 @@
+// frontend/src/store/types/index.js
+
+export * from "./chatbotTypes";
+export * from "./userTypes";
+export * from "./analyticsTypes";
+export * from "./automationTypes";
+export * from "./marketplaceTypes";
+export * from "./settingsTypes";
+export * from "./notificationsTypes";
+export * from "./integrationsTypes";
+export * from "./securityTypes";
