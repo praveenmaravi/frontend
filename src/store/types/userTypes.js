dc6aa5fb986a416c3257aee9ad90c4cebed05a3f
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 12:19:57 +0530

    Create userTypes.js

---
+++ src/store/types/userTypes.js
@@ -0,0 +1,9 @@
+// frontend/src/store/types/userTypes.js
+
+export const USER_LOGIN = "USER_LOGIN";
+export const USER_LOGOUT = "USER_LOGOUT";
+export const USER_REGISTER = "USER_REGISTER";
+export const USER_SET_PROFILE = "USER_SET_PROFILE";
+export const USER_UPDATE_PREFERENCES = "USER_UPDATE_PREFERENCES";
+export const USER_SET_LOADING = "USER_SET_LOADING";
+export const USER_SET_ERROR = "USER_SET_ERROR";
