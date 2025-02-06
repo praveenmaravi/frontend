8a632a053e8f071a706def8c90efb9185b57dc1d
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 13:59:11 +0530

    Update ChatbotAvatar.js

--- src/views/ChatbotInterface/components/ChatbotAvatar/ChatbotAvatar.js
+++ src/views/ChatbotInterface/components/ChatbotAvatar/ChatbotAvatar.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import './ChatbotAvatar.css';
+import './style/ChatbotAvatar.css';
 
 const ChatbotAvatar = ({ avatarUrl, altText }) => {
   return (
