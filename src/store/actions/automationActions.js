bc3f8c34f0219d8be707d0cc65985db393a2bccb
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 11:27:33 +0530

    Update automationActions.js

--- src/store/actions/automationActions.js
+++ src/store/actions/automationActions.js
@@ -1,1 +1,28 @@
+import { FETCH_AUTOMATIONS, TRIGGER_AUTOMATION, SET_AUTOMATION_ERROR } from "../types";
+import api from "../../services/api";
+
+/**
+ * Fetch all automation workflows
+ */
+export const fetchAutomations = () => async (dispatch) => {
+    try {
+        const response = await api.get("/automation");
+        dispatch({ type: FETCH_AUTOMATIONS, payload: response.data });
+    } catch (error) {
+        dispatch({ type: SET_AUTOMATION_ERROR, payload: error.message });
+    }
+};
+
+/**
+ * Trigger an automation workflow by ID
+ * @param {string} automationId - The ID of the automation to trigger
+ */
+export const triggerAutomation = (automationId) => async (dispatch) => {
+    try {
+        const response = await api.post(`/automation/trigger/${automationId}`);
+        dispatch({ type: TRIGGER_AUTOMATION, payload: response.data });
+    } catch (error) {
+        dispatch({ type: SET_AUTOMATION_ERROR, payload: error.message });
+    }
+};
 
