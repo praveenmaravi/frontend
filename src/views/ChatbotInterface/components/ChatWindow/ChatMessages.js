bd65e90bf46ec82fd333020eb5db569f235d2232
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 13:55:51 +0530

    Update ChatMessages.js

--- src/views/ChatbotInterface/components/ChatWindow/ChatMessages.js
+++ src/views/ChatbotInterface/components/ChatWindow/ChatMessages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import MessageInput from './MessageInput'; // A reusable component for individual messages
-import { selectMessages } from '../../store/chatbotSlice'; // Redux selector for chatbot messages
+import { selectMessages } from '../../../../store/chatbotSlice'; // Redux selector for chatbot messages
 
 const ChatMessages = () => {
   // Fetch messages from the Redux store
