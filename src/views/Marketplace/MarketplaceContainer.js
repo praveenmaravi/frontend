4644b399d463a5d6bbe540ac268855618461a1ab
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 13:43:16 +0530

    Update MarketplaceContainer.js

--- src/views/Marketplace/MarketplaceContainer.js
+++ src/views/Marketplace/MarketplaceContainer.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import MarketplacePage from "./MarketplacePage";
-import { useMarketplaceItems } from "../hooks/useMarketplaceItems";
-import { useCategoryFilter } from "../hooks/useCategoryFilter";
-import { useSortItems } from "../hooks/useSortItems";
-import { useSearch } from "../hooks/useSearch";
+import { useMarketplaceItems } from "./hooks/useMarketplaceItems";
+import { useCategoryFilter } from "./hooks/useCategoryFilter";
+import { useSortItems } from "./hooks/useSortItems";
+import { useSearch } from "./hooks/useSearch";
 
 const MarketplaceContainer = () => {
   const { items, fetchItems, loading } = useMarketplaceItems();
