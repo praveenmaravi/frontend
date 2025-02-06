78a80ddb187af19f7c96358bef03095a31da613c
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 11:56:17 +0530

    Create index.js

---
+++ src/store/selectors/index.js
@@ -0,0 +1,11 @@
+// frontend/src/store/selectors/index.js
+
+export * from './chatbotSelectors';
+export * from './userSelectors';
+export * from './analyticsSelectors';
+export * from './settingsSelectors';
+export * from './marketplaceSelectors';
+export * from './automationSelectors';
+export * from './notificationsSelectors';
+export * from './integrationsSelectors';
+export * from './securitySelectors';
