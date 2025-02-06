199c64b969e8733aa037313db0fb59c1a77f1c55
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-06 14:43:58 +0530

    Create index.js

---
+++ src/services/api/index.js
@@ -0,0 +1,20 @@
+import analyticsService from './analyticsService';
+import automationService from './automationService';
+import chatbotService from './chatbotService';
+import documentService from './documentService';
+import integrationsService from './integrationsService';
+import notificationService from './notificationService';
+import settingsService from './settingsService';
+import userService from './userService';
+
+// Export all services from a single file for easier imports
+export {
+  analyticsService,
+  automationService,
+  chatbotService,
+  documentService,
+  integrationsService,
+  notificationService,
+  settingsService,
+  userService,
+};
