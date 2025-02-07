a8555b0c996fd5202441db98096658714d5722e4
praveenmaravi <praveenmaravi2006@gmail.com>
2025-02-07 11:07:30 +0530

    Update useMarketplaceItems.js

--- src/views/Marketplace/hooks/useMarketplaceItems.js
+++ src/views/Marketplace/hooks/useMarketplaceItems.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setMarketplaceItems, setLoading, setError } from '../../../store/marketplaceSlice';
+import { setMarketplaceItems, setLoading, setError } from '../../store/marketplaceSlice';
 import { fetchMarketplaceItems } from '../../../services/api/marketplaceAPI';
 
 /**
